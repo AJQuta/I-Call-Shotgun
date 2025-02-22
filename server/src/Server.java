34b79ee8179dd4ad72e9c8ded194c2b4e905dbc2
AJQuta <ajquta@purdue.edu>
2025-02-22 09:22:48 -0500

    Setting up threads

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,22 +1,45 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.util.LinkedList;
 
 public class Server {
 
+    class Thread {
+        private int thr_port;
+        private ServerSocket thr_serv;
+
+        protected Thread(int p) {
+            thr_port = p;
+            try {
+                thr_serv = new ServerSocket(thr_port);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+
+        
+    }
+
+    private int port;
+    private LinkedList<Thread> socket_threads;
     private Socket sock;
     private ServerSocket servSock;
-
+    
     public Server() {
-
+        port = 4444;
         try {
-            this.servSock = new ServerSocket(4444);
+            this.servSock = new ServerSocket(port);
         } catch (IOException e) {
             e.printStackTrace();
             return;
         }
         
+    }
+
+    private Socket create_serv_thread() {
+
     }
 
     public static void main(String[] args) {
