0dd0013b6f65a4f4a589f3cf4ab57455188a496d
AJQuta <ajquta@purdue.edu>
2025-02-21 22:24:16 -0500

    trying to setup server...

--- server/src/Server.java
+++ server/src/Server.java
@@ -5,8 +5,30 @@ import java.net.Socket;
 
 public class Server {
 
+    private Socket sock;
+    private ServerSocket servSock;
+
     public Server() {
+
+        try {
+            this.servSock = new ServerSocket(4444);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
         
+    }
+
+    public static void main(String[] args) {
+        Server s = new Server();
+
+        while (true) {
+            try {
+                s.sock = s.servSock.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
     
 }
